80b7b3bba7ece33d6db6865521204b3d425e25d2
Joe Wong <wongjoseph10@yahoo.com>
2021-12-16 20:00:58 -0800

    add break when end of data

--- dummy_server.py
+++ dummy_server.py
@@ -1,18 +1,17 @@
 import socket
+import time
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 7777
-BUFFER_SIZE = 20
+BUFFER_SIZE = 1024
 
+print("starting server...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-
+s.listen()
 conn, addr = s.accept()
-print("starting server at: ", addr)
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    print("received data: ", data)
-    conn.send(data)
-conn.close()
+    else:
+        print("received data: ", data)
