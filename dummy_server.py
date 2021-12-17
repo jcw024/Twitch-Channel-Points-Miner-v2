5a92649ef322ece415654640db779cec12f91da3
Joe Wong <wongjoseph10@yahoo.com>
2021-12-16 21:50:01 -0800

    update dummy server for multiple clients

--- dummy_server.py
+++ dummy_server.py
@@ -1,17 +1,26 @@
 import socket
+from threading import Thread
 import time
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 7777
 BUFFER_SIZE = 1024
 
+def on_new_client(conn, addr):
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        print(data, flush=True)
+        if not data: break
+        conn.sendall(data)
+    print(addr, " closed", flush=True)
+    conn.close()
+
 print("starting server...")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen()
-conn, addr = s.accept()
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    else:
-        print("received data: ", data)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen()
+    while 1:
+        conn, addr = s.accept()
+        print(addr, " connected")
+        t = Thread(target=on_new_client, args=(conn,addr))
+        t.start()
