51f316b07047512b2b343e7e483b63cefaf63bc9
Joe Wong <wongjoseph10@yahoo.com>
2021-12-24 18:20:02 -0800

    switch from sparkcontext to spark session with structured streaming

--- dummy_spark_client.py
+++ dummy_spark_client.py
@@ -1,12 +1,37 @@
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
+#from pyspark import SparkContext
+#from pyspark.streaming import StreamingContext
+from pyspark.sql import SparkSession
 
-sc = SparkContext("local[2]", "dummy_spark_server")
-sc.setLogLevel("ERROR")
+spark = SparkSession.builder \
+        .master("local[2]") \
+        .appName("dummy_spark_server") \
+        .getOrCreate()
+
+lines = spark \
+        .readStream \
+        .format("socket") \
+        .option("host", "localhost") \
+        .option("port", 7778) \
+        .load()
+
+
+query = lines \
+        .writeStream \
+        .outputMode("append") \
+        .format("console") \
+        .start()
+
+query.awaitTermination()
+
+"""
+
+#sc.setLogLevel("ERROR")
 ssc = StreamingContext(sc, 10)
 
 data = ssc.socketTextStream("127.0.0.1",7778)
+#data.foreachRDD(lambda x: )
 data.pprint(10)
 
 ssc.start()
 ssc.awaitTermination()
+"""
